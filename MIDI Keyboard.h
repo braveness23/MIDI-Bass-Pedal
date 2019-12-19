0165f66d1d7e96c2fa61398f795ee3742f2ddbd8
Dave Schwinn <dschwinn@cngholdings.com>
2019-12-19 17:19:02 -0500

    added untracked file

---
+++ MIDI Keyboard.h
@@ -0,0 +1,19 @@
+class MIDIKeyboard
+{
+private:
+    int num_keys;
+    int octave;
+    bool latch;
+
+public:
+    MIDI Keyboard(/* args */);
+    ~MIDI Keyboard();
+};
+
+MIDIKeyboard::MIDIKeyboard(/* args */)
+{
+}
+
+MIDIKeyboard::~MIDIKeyboard()
+{
+}
